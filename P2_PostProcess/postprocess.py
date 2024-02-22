11c40710f8ab846d1aa08d0fb2d19cae54d024af
hclark94 <harrydclark91@gmail.com>
2024-02-22 22:47:56 +0000

    add postprocess

--- P2_PostProcess/postprocess.py
+++ P2_PostProcess/postprocess.py
@@ -2,6 +2,24 @@ from Helpers.upload_download import *
 
 def postprocess(working_recording_path, local_path, **kwargs):
     recording_paths = get_recordings_to_postprocess(working_recording_path, local_path, **kwargs)
+    recording_types = get_recording_types(recording_paths)
+
+    for recording_path, type in zip(recording_paths, recording_types):
+        if type == "vr":
+            print("do something")
+            # do stuff to do with vr recordings
+        elif type == "openfield":
+            print("do something")
+            # do stuff to do with open field recordings
+        elif type == "opto":
+            print("do something")
+            # do stuff to do with opto recordings
+        elif type == "sleep":
+            print("do something")
+            # do stuff to do with sleep recordings
+        else:
+            print(type, " isn't a recognised recording "
+                        "type in postprocessing ")
 
     # then do stuff
     return
