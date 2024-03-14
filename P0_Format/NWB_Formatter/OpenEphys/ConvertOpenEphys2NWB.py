2032ea4d32e4b834a4094595e974d54752df4a60
hclark94 <harrydclark91@gmail.com>
2024-03-14 15:20:51 +0000

    add formatting to nwb

--- P0_Format/NWB_Formatter/OpenEphys/ConvertOpenEphys2NWB.py
+++ P0_Format/NWB_Formatter/OpenEphys/ConvertOpenEphys2NWB.py
@@ -1,132 +1,90 @@
 # make sure python-neo is installed in the virtual environment buy navigating within /python-neo and installing with "pip install ."
 # Note: this python-neo is a fork of the original that affords can read OpenEphys legacy data without .event films
 import os
-from neuroconv.utils.dict import load_dict_from_file, dict_deep_update
+from neuroconv.utils.dict import load_dict_from_file
 from neuroconv.datainterfaces import OpenEphysRecordingInterface
+from neuroconv import ConverterPipe
+import pandas as pd
 import numpy as np
+from dateutil import tz
+import uuid
+from datetime import datetime
+from pynwb import NWBHDF5IO, NWBFile, TimeSeries
 
-def make_yaml_from_schema(interface, metadata_path, descriptions=True, existing_data=None):
-    the_schema = interface.get_metadata_schema()
 
-    metadata_file = open(metadata_path, 'w')
+def get_age_at_recording(session_start_time, date_of_birth):
+    age = session_start_time - date_of_birth
+    age_in_days = age.total_seconds()/(60*60*24)
+    return int(age_in_days)
 
-    for level1key in list(the_schema['properties'].keys()):
-        print(f"{level1key}:", file=metadata_file)
 
-        level2keys = list(the_schema['properties'][level1key]['properties'].keys())
-        if 'definitions' in level2keys:
-            level2keys.remove('definitions')
+def get_dob(subject_id, dob_csv):
+    dob = dob_csv[dob_csv["id"] == subject_id]["dob"].iloc[0]
+    dob.to_pydatetime()
+    return dob
 
-        for level2key in level2keys:
-            print(f"  {level2key}: ", end="", file=metadata_file)
-
-            if existing_data is not None and level2key in existing_data[level1key] and type(
-                    existing_data[level1key][level2key]) == str:
-                print(f"{existing_data[level1key][level2key]}", end="", file=metadata_file)
-
-            if descriptions == True:
-                try:
-                    print(f" # {the_schema['properties'][level1key]['properties'][level2key]['description']}",
-                          file=metadata_file, end="")
-                except:
-                    if level1key != 'Ecephys':
-                        print(f" # No description given", file=metadata_file, end="")
-
-            print("", file=metadata_file)
-
-            if the_schema['properties'][level1key]['properties'][level2key]['type'] == 'array':
-
-                if '$ref' in the_schema['properties'][level1key]['properties'][level2key]['items']:
-                    print(f"    - {{\n", end="", file=metadata_file)
-                    for level3key in the_schema['properties']['Ecephys']['properties']['definitions'][level2key][
-                        'properties'].keys():
-
-                        print(f"    {level3key}: ,", file=metadata_file, end="")
-                        # print(f"{level1key}, {level2key}, {level3key}", file=metadata_file, end="")
-
-                        if descriptions == True:
-                            try:
-                                print(
-                                    f" # {the_schema['properties'][level1key]['properties']['definitions'][level2key]['properties'][level3key]['description']}",
-                                    file=metadata_file, end="")
-                            except:
-                                print(f" # No description given", file=metadata_file, end="")
-                        print("", file=metadata_file)
-
-                    print(f"}}", file=metadata_file)
-
-                else:
-                    print(f"    -", file=metadata_file)
-
-            if level2key == 'ElectricalSeries':
-                # print("", file=metadata_file)
-
-                for level3key in list(
-                        the_schema['properties'][level1key]['properties'][level2key]['properties'].keys()):
-                    print(f"    {level3key}: ", end="", file=metadata_file)
-
-                    if existing_data is not None and level3key in existing_data[level1key][level2key] and type(
-                            existing_data[level1key][level2key][level3key]) == str:
-                        print(f"{existing_data[level1key][level2key][level3key]}", end="", file=metadata_file)
-
-                    if descriptions == True:
-                        try:
-                            print(
-                                f" # {the_schema['properties'][level1key]['properties'][level2key]['properties'][level3key]['description']}",
-                                file=metadata_file, end="")
-                        except:
-                            print(f" # No description given", file=metadata_file, end="")
-
-                    print("", file=metadata_file)
-    metadata_file.close()
 
 def convert(recording_path, processed_folder_name, **kwargs):
+    # handle save locations and paths
     recording_name = os.path.basename(recording_path)
+    nwb_path = recording_path+"/"+processed_folder_name+"/nwb/" # Where to save the .nwb file
+    nwb_file_name = nwb_path+recording_name+".nwb"
+    if not os.path.exists(nwb_path):
+        os.mkdir(nwb_path)
 
-    if not os.path.exists(recording_path + "/nwb/"):
-        os.mkdir(recording_path + "/nwb/")
-
-    my_ephys_interface = OpenEphysRecordingInterface(folder_path=recording_path, stream_name="Signals CH")
-    existing_data = my_ephys_interface.get_metadata()
-
+    # handle metadata
     metadata_path = "/mnt/datastore/Harry/basic_metadata.yml" # basic
-    nwbfile_path = recording_path+"/"+processed_folder_name+"/nwb/"+recording_name+".nwb"  # Where to save the .nwb file
-    #make_yaml_from_schema(my_ephys_interface, metadata_path, descriptions=True, existing_data=existing_data)
-    metadata = load_dict_from_file(file_path=metadata_path)
-    """
-    metadata["NWBFile"]["keywords"] = ["",""]
-    metadata["NWBFile"]["related_publications"] = ["", ""]
-    metadata["NWBFile"]["experiment_description"] = "unlassified1"
-    metadata["NWBFile"]["experimenter"] = ["", ""]
-    metadata['NWBFile']['session_id'] = "unclassified4"
-    metadata['NWBFile']['institution'] = "unclassified5"
-    metadata['NWBFile']['lab'] = "unclassified6"
-    metadata['NWBFile']['session_start_time'] = "2000-10-31T01:30:00.000-05:00"
-    metadata['NWBFile']['surgery'] = "unclassified7"
-    metadata['NWBFile']['pharmacology'] = "unclassified8"
-    metadata['NWBFile']['protocol'] = "unclassified9"
-    metadata['NWBFile']['surgery'] = "unclassified10"
-    metadata['NWBFile']['slices'] = "unclassified11"
-    metadata['NWBFile']['source_script'] = "unclassified12"
-    metadata['NWBFile']['source_script_file_name'] = "unclassified13"
-    metadata['NWBFile']['notes'] = "unclassified14"
-    metadata['NWBFile']['virus'] = "unclassified15"
-    metadata['NWBFile']['data_collection'] = "unclassified18"
-    metadata['NWBFile']['stimulus_notes'] = "unclassified19"
-    metadata["Subject"]["subject_id"] = "unclassified3"
-    metadata["Subject"]["species"] = "Pachyuromys duprasi"
-    metadata["Subject"]["description"] = "unclassified3"
-    metadata["Subject"]["genotype"] = "unclassified3"
-    metadata["Subject"]["strain"] = "unclassified3"
-    metadata["Subject"]["sex"] = "M"
-    metadata["Subject"]["weight"] = "unclassified3"
-    metadata["Subject"]["date_of_birth"] = "2000-10-31T01:30:00.000-05:00"
-    metadata["Subject"]["age"] = "unclassified3"
-    metadata["Subject"]["age__reference"] = "birth"
-    """
-    if isinstance(metadata, dict):
-        my_ephys_interface.run_conversion(nwbfile_path=nwbfile_path, metadata=metadata)
+    path_to_dob_csv = "/mnt/datastore/Harry/Grid_anchoring_eLife_2023/mouse_dobs.csv"
+    params = load_dict_from_file(recording_path+"/params.yml")
+    subject_id = recording_path.split("/")[-1].split("_")[0]
+    session_id = recording_path.split("/")[-1]
+    session_description = params["recording_type"]
+    st = ("-".join(session_id.split("_")[2:])).split("-")
+    st = datetime(int(st[0]),int(st[1]), int(st[2]), int(st[3]),int(st[4]), int(st[5]), tzinfo=tz.gettz("UK/GMT"))
+    dob_csv = pd.read_csv(path_to_dob_csv, parse_dates=["dob"])
+
+    if subject_id in np.asarray(dob_csv["id"]):
+        if not os.path.exists(nwb_file_name):
+            date_of_birth = get_dob(subject_id, dob_csv)
+            age = get_age_at_recording(st, date_of_birth)
+            metadata = load_dict_from_file(file_path=metadata_path)
+            metadata["NWBFile"]["identifier"] = str(uuid.uuid4())
+            metadata["NWBFile"]["session_id"] = session_id
+            metadata["NWBFile"]["session_start_time"] = st
+            metadata["NWBFile"]["session_description"] = session_description
+            metadata["Subject"]["subject_id"] = subject_id
+            metadata["Subject"]["age"] = "P"+str(age)
+            metadata["Subject"]["date_of_birth"] = date_of_birth
+            metadata["Ecephys"]["ElectricalSeries"]["starting_time"] = 0
+
+            # load ephys and convert to nwb
+            ephys_interface = OpenEphysRecordingInterface(folder_path=recording_path, stream_name="Signals CH", verbose=True)
+            converter = ConverterPipe(data_interfaces=[ephys_interface], verbose=True) # add interfaces to the list if needed
+            converter.run_conversion(nwbfile_path=nwb_file_name, metadata=metadata, overwrite=True)
+
+            # append nwb with behaviour data
+            io = NWBHDF5IO(nwb_file_name, mode="a")
+            nwbfile = io.read()
+
+            behaviour_csv = pd.read_csv(recording_path + "/processed/position_data.csv")
+            timestamps = np.asarray(behaviour_csv["time_seconds"])
+            del behaviour_csv["time_seconds"]
+            for column in list(behaviour_csv):
+                if "Unnamed" in column:
+                    continue # do nothing with this junk column
+                else:
+                    behavioral_time_series = TimeSeries(name=column,
+                                                        data=np.asarray(behaviour_csv[column]),
+                                                        timestamps=timestamps,
+                                                        description="position: cm, speed: cm/s, angle: degrees, trials: non-zero indexed, "
+                                                                    "trial types: 0-beaconed, 1-nonbeaconed, 2-probe",
+                                                        unit="n/a")
+                    nwbfile.add_acquisition(behavioral_time_series)
+            io.write(nwbfile)
+            io.close()
+            print("I have completed nwb conversion for ", recording_name)
     return
+
 
 def main():
     print("")
