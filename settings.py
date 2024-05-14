fc89b2bc6771f571237ca98f24f59b5083d66426
hclark94 <harrydclark91@gmail.com>
2024-05-14 12:59:37 +0100

    add mountainsort5 to list of named sorters

--- settings.py
+++ settings.py
@@ -18,8 +18,8 @@ down_sampled_rate = 1000
 #########
 # Sorter Configuration
 sorterName = 'mountainsort4'
-list_of_named_sorters = ['MountainSort', 'mountainsort4','klusta','tridesclous','hdsort','ironclust',
-                         'kilosort','kilosort2', 'spykingcircus','herdingspikes','waveclus']
+list_of_named_sorters = ['MountainSort', 'mountainsort4','mountainsort5','klusta','tridesclous','hdsort','ironclust',
+                         'kilosort','kilosort2', 'kilosort3', 'kilosort4', 'spykingcircus','herdingspikes','waveclus']
 
 whiten = True
 common_reference = True
