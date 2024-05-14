f23a8cc00287857c719204d2e76f62990b9bd0fa
hclark94 <harrydclark91@gmail.com>
2024-05-14 12:57:57 +0100

    comment out n_workers for spike sorting parameters

--- P1_SpikeSort/preprocess.py
+++ P1_SpikeSort/preprocess.py
@@ -5,7 +5,7 @@ import spikeinterface.full as si
 def ammend_preprocessing_parameters(params):
     params["whiten"] = settings.whiten
     params["filter"] = not settings.bandpass_filter
-    params['num_workers'] = settings.n_sorting_workers
+    #params['num_workers'] = settings.n_sorting_workers
     return params
 
 def preprocess(recording):
