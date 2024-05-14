fbb302aa594d7ad64438ee37e20ad43533948be6
hclark94 <harrydclark91@gmail.com>
2024-05-14 13:10:39 +0100

    remove unnamed columns in blender dataframe and add check for .continous files before attempting to generate position datra from the adc channels

--- P2_PostProcess/VirtualReality/vr.py
+++ P2_PostProcess/VirtualReality/vr.py
@@ -15,13 +15,21 @@ def process(recording_path, processed_folder_name, **kwargs):
         position_data = pd.read_csv(recording_path+"/"+processed_folder_name+"/position_data.csv")
     elif np.any(["blender.csv" in f.name and f.is_file() for f in files]):
         position_data = generate_position_data_from_blender_file(recording_path, processed_folder_name)
-    else:
+    elif np.any([".continuous" in f.name and f.is_file() for f in files]):
         position_data = generate_position_data_from_ADC_channels(recording_path, processed_folder_name)
+    else:
+        print("I couldn't find any source of position data")
+        return
+
     print("I am using position data with an avg sampling rate of ", str(1/np.nanmean(np.diff(position_data["time_seconds"]))), "Hz")
     # add a step for syncing data if necesssary
     # TODO position_data = sync_posi...
     # process video
-    position_data = process_video(recording_path, processed_folder_name, position_data)
+    #position_data = process_video(recording_path, processed_folder_name, position_data)
+
+    for column in list(position_data):
+        if "Unnamed" in column:
+            del position_data[column]
 
     # save position data
     position_data.to_csv(recording_path + "/" + processed_folder_name + "/position_data.csv", index=False)
