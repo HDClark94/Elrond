616b0400915bb75b19c8a89a4c807d7689491697
hclark94 <harrydclark91@gmail.com>
2024-05-17 10:46:52 +0100

    add optional plot lfp kwarg

--- P2_PostProcess/Shared/shared.py
+++ P2_PostProcess/Shared/shared.py
@@ -10,14 +10,13 @@ def process(recording_path, processed_folder_name, **kwargs):
     #makes plots and/or dataframes and add them to the processed_recording folder
 
     # plot tracess for spikes and channels
-
     make_spike_trace_summary(recording_path, processed_folder_name)
-    make_channel_trace_summary(recording_path, processed_folder_name, filtered=False)
-    make_channel_trace_summary(recording_path, processed_folder_name, filtered=True)
-    make_lfp_trace_summary(recording_path, processed_folder_name)
+    #make_channel_trace_summary(recording_path, processed_folder_name, filtered=False)
+    #make_channel_trace_summary(recording_path, processed_folder_name, filtered=True)
+    if "plot_lfp" in kwargs.keys():
+        if kwargs["plot_lfp"] == True:
+            make_lfp_trace_summary(recording_path, processed_folder_name)
 
     make_pdf_from_recording(recording_path, processed_folder_name)
-
-
     return
 
