85b4b365d65fc705b7f56c12f685897c0f995e77
Chris Halcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-05-17 11:10:30 +0100

    add postprocess_behaviour_only flag in kwargs

--- run_scripts/run_pipeline.py
+++ run_scripts/run_pipeline.py
@@ -32,6 +32,7 @@ def process_recordings(recording_paths, local_path="", processed_folder_name="",
         save2phy: flag whether to export_to_phy after spike sorting (useful for manual curation)
         use_dlc_to_extract_openfield_position: flag whether to ignore any processed output from bonsai and instead use
         deeplabcut to extract openfield position from the raw video
+        postprocess_behaviour_only: flag whether to postprocess only the behavioural data
         sorterName: string for a named sorted if spikesorting is called, options include:
         'mountainsort4','klusta','tridesclous','hdsort','ironclust','kilosort',
         'kilosort2', 'spykingcircus','herdingspikes','waveclus'. For each sorter, a different set up might be required
@@ -89,38 +90,21 @@ def main():
     # to grab a whole directory of recordings
 
     recording_paths = []
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort8_may2021/vr") if f.is_dir()])
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort7_october2020/vr") if f.is_dir()])
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort6_july2020/vr") if f.is_dir()])
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort8_may2021/of") if f.is_dir()])
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort7_october2020/of") if f.is_dir()])
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort6_july2020/of") if f.is_dir()])
-    #recording_paths = ["/mnt/datastore/Harry/test_recording/vr/M11_D36_2021-06-28_12-04-36"] # example vr tetrode session with a linked of session
-    #recording_paths = ["/mnt/datastore/Harry/test_recording/vr/M18_D1_2023-10-30_12-38-29"] # example vr cambridge p1 probe session with a linked of session (2 x 64 channels)
-    #recording_paths = ["/mnt/datastore/Harry/Cohort9_february2023/vr/M16_D1_2023-02-28_17-42-27"]
-    #recording_paths =["/mnt/datastore/Harry/test_recording/vr/M0_D0_just_behaviour"]
-    #recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/cohort11_april2024/vr") if f.is_dir()])
-    #recording_paths = ["/run/user/1000/gvfs/smb-share:server=cmvm.datastore.ed.ac.uk,share=cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/"
-    #                   "ActiveProjects/Harry/Cohort11_april2024/vr/M19_D9_2024-05-04_14-07-27"]
+    recording_paths.extend([f.path for f in os.scandir("/run/user/1000/gvfs/smb-share:server=cmvm.datastore.ed.ac.uk,"
+                                                       "share=cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/"
+                                                       "Harry/Cohort11_april2024/of") if f.is_dir()])
 
-    recording_paths.extend([f.path for f in os.scandir("/mnt/datastore/Harry/Cohort11_april2024/of/") if f.is_dir()])
-    recording_paths = ["/mnt/datastore/Harry/Cohort11_april2024/vr/M20_D14_2024-05-13_16-45-26_VR1",
-                       "/mnt/datastore/Harry/Cohort11_april2024/vr/M21_D15_2024-05-15_11-54-28_VR1",
-                       "/mnt/datastore/Harry/Cohort11_april2024/vr/M20_D15_2024-05-14_15-15-41_VR1",
-                       "/mnt/datastore/Harry/Cohort11_april2024/vr/M21_D16_2024-05-16_14-40-02_VR1",
-                       "/mnt/datastore/Harry/Cohort11_april2024/vr/M20_D16_2024-05-15_16-16-44_VR1",
-                       "/mnt/datastore/Harry/Cohort11_april2024/vr/M20_D17_2024-05-16_16-33-37_VR1"]
-    #recording_paths = ["/mnt/datastore/Harry/test_recording/of/M11_D36_2021-06-28_11-19-21"]
     process_recordings(recording_paths,
                        local_path="/home/ubuntu/to_sort/recordings/",
                        processed_folder_name="processed",
                        copy_locally=False,
-                       run_spikesorting=True,
+                       run_spikesorting=False,
                        update_results_from_phy=False,
-                       run_postprocessing=False,
-                       concat_sort=True,
+                       run_postprocessing=True,
+                       concat_sort=False,
                        postprocess_based_on_concat_sort=False,
-                       save2phy=True,
+                       postprocess_behaviour_only=True,
+                       save2phy=False,
                        use_dlc_to_extract_openfield_position=True,
                        sorterName="mountainsort5",
                        sorter_kwargs={"scheme": "3"})
