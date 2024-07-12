b5bd2bfc7a6d7d3e95bb294d011558a3bc11d341
hclark94 <harrydclark91@gmail.com>
2024-07-12 17:01:13 +0100

    add python paths to eddie bash

--- Helpers/Eddie_scripts/eddie_stagein.sh
+++ Helpers/Eddie_scripts/eddie_stagein.sh
@@ -11,4 +11,12 @@
 # Hard runtime limit
 #$ -l h_rt=00:10:00
 
+# Load modules if required
+# e.g. module load <name>/<version>
+module load anaconda/2024.02
+conda activate /exports/eddie/scratch/hclark3/anaconda/envs/si
+
+# append the python path
+export PYTHONPATH="/home/hclark3/Elrond"
+
 python /home/hclark3/Elrond/Helpers/Eddie_scripts/eddie_stagein.py
