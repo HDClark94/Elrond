e8b0733803c106f65df7532f71ebcabf950b85fb
hclark94 <harrydclark91@gmail.com>
2024-07-13 22:45:31 +0100

    add indentations

--- run_scripts/run_pipeline_on_eddie.py
+++ run_scripts/run_pipeline_on_eddie.py
@@ -14,29 +14,30 @@ download_job_name = os.path.basename(recording_to_process)[:7]+"_DL"
 pipeline_job_name = os.path.basename(recording_to_process)[:7]+"_PL"
 upload_job_name = os.path.basename(recording_to_process)[:7]+"_UL"
 
-subprocess.check_call(f'qsub -N {download_job_name} '
-                      f'-v RECORDING_PATH={recording_to_process} '
-                      f'-v LOCAL_SCRATCH_PATH={LOCAL_SCRATCH_PATH} '
-                      f'{STAGEIN_PATH}', shell=True)
+
+subprocess.check_call(f'qsub -N {download_job_name} '\
+         f'-v RECORDING_PATH={recording_to_process} '\
+         f'-v LOCAL_SCRATCH_PATH={LOCAL_SCRATCH_PATH} '\
+         f'{STAGEIN_PATH}', shell=True)
 print("submitted job ", download_job_name)
 
-subprocess.check_call(f'qsub -N {pipeline_job_name} '
-                      f'-hold_jid {download_job_name} '
-                      f'-v RECORDING_PATH={recording_to_process} '
-                      f'-v LOCAL_PATH={LOCAL_SCRATCH_PATH} '
-                      f'-v SPIKESORT={True} '
-                      f'-v UPDATE_FROM_PHY={False} '
-                      f'-v POSTPROCESS={True} '
-                      f'-v CONCATSORT={True} '
-                      f'-v DLC={True} '
-                      f'-v SORTER={"kilosort4"} '
+subprocess.check_call(f'qsub -N {pipeline_job_name} '\
+                      f'-hold_jid {download_job_name} '\
+                      f'-v RECORDING_PATH={recording_to_process} '\
+                      f'-v LOCAL_PATH={LOCAL_SCRATCH_PATH} '\
+                      f'-v SPIKESORT={True} '\
+                      f'-v UPDATE_FROM_PHY={False} '\
+                      f'-v POSTPROCESS={True} '\
+                      f'-v CONCATSORT={True} '\
+                      f'-v DLC={True} '\
+                      f'-v SORTER={"kilosort4"} '\
                       f'{PIPELINE_PATH}', shell=True)
 print("submitted job ", pipeline_job_name)
 
-subprocess.check_call(f'qsub -N {upload_job_name} '
-                      f'-hold_jid {pipeline_job_name} '
-                      f'-v RECORDING_PATH={recording_to_process} '
-                      f'-v LOCAL_SCRATCH_PATH={LOCAL_SCRATCH_PATH} '
+subprocess.check_call(f'qsub -N {upload_job_name} '\
+                      f'-hold_jid {pipeline_job_name} '\
+                      f'-v RECORDING_PATH={recording_to_process} '\
+                      f'-v LOCAL_SCRATCH_PATH={LOCAL_SCRATCH_PATH} '\
                       f'{STAGEOUT_PATH}', shell=True)
 print("submitted job ", upload_job_name)
 
