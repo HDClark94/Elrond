cd8d8dc9b01b27bd0c67793d3ed32b3f0253804d
Teris Tam <harrydclark91@gmail.com>
2024-09-05 15:20:11 +0000

    add parallel ramp score

--- P3_CurrentAnalysis/ramp_score.py
+++ P3_CurrentAnalysis/ramp_score.py
@@ -1,11 +1,14 @@
-
 import numpy as np 
 import settings
-import pandas as pd
+import pandas as pd 
 import scipy.stats as stats
 import statsmodels.api as sm
 from statsmodels.stats.multitest import fdrcorrection
 from P2_PostProcess.VirtualReality.spatial_firing import add_kinematics, bin_fr_in_space
+from joblib import Parallel, delayed
+import multiprocessing
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def shuffled_times(cluster_firing, position_data, n_shuffles=1000):
@@ -82,7 +85,7 @@ def calculate_ramp_scores(spike_data, processed_position_data,
     model_params = pd.DataFrame() 
     for cluster_index, cluster_id in enumerate(spike_data.cluster_id):
         cluster_df = spike_data[spike_data["cluster_id"]==cluster_id]
- 
+
         # shuffle the firing times based on a cyclic shuffling procedure, we add any time between 20 seconds to 10 minutes for all spikes
         shuffle_firing = shuffled_times(cluster_df, position_data, n_shuffles=n_shuffles) 
         shuffle_firing = add_kinematics(shuffle_firing, position_data)
@@ -140,3 +143,80 @@ def calculate_ramp_scores(spike_data, processed_position_data,
     if save:
         model_params.to_pickle(save_path+"ramp_classifications.pkl")
     return model_params
+
+
+def ramp_parallel(cluster_df, processed_position_data, position_data, track_length, alpha, n_shuffles):
+    cluster_df = cluster_df.to_frame().T.reset_index(drop=True)
+    cluster_id = cluster_df["cluster_id"].iloc[0]
+
+    # shuffle the firing times based on a cyclic shuffling procedure, we add any time between 20 seconds to 10 minutes for all spikes
+    shuffle_firing = shuffled_times(cluster_df, position_data, n_shuffles=n_shuffles) 
+    shuffle_firing = add_kinematics(shuffle_firing, position_data)
+    shuffle_firing = bin_fr_in_space(shuffle_firing, position_data, 
+                                     track_length=track_length, smoothen=False) # dont smoothen as this can introduce false ramps 
+
+    # calculate scores across all trial types, trial performances and track-regions
+    model_params = pd.DataFrame() 
+    for trial_type in [None, 0,1,2]: 
+        for hit_miss_try in [None, "hit", "try", "run"]:
+            for track_region in [None, "full", "outbound", "homebound"]:
+                #shuffle_params_df = pd.DataFrame(columns=["slope", "pval"])
+                params = ramp_score(np.array(cluster_df["fr_binned_in_space"].iloc[0]),
+                                    processed_position_data=processed_position_data,
+                                    trial_type=trial_type,
+                                    hit_miss_try=hit_miss_try,
+                                    track_region=track_region)
+
+                params_df = pd.DataFrame({'shuffle_id': [-1],
+                                            'slope': [params[0]], 
+                                            'p_val': [params[1]]})  
+                
+                for sh_i, shuffle in shuffle_firing.iterrows():
+                    shuffle_params = ramp_score(np.array(shuffle["fr_binned_in_space"]),
+                                                processed_position_data=processed_position_data,
+                                                trial_type=trial_type,
+                                                hit_miss_try=hit_miss_try,
+                                                track_region=track_region)
+                    shuffle_params_df = pd.DataFrame({'shuffle_id': [sh_i],
+                                                        'slope': [shuffle_params[0]], 
+                                                        'p_val': [shuffle_params[1]]}) 
+                    
+                    params_df = pd.concat([params_df, shuffle_params_df], ignore_index=True)  
+                
+                #Correct pvalues
+                _, params_df["p_val"] = fdrcorrection(params_df["p_val"])
+
+                #calculate percentile upper and lower bound limits 
+                lower = np.nanpercentile(params_df["slope"][1:], 5)
+                upper = np.nanpercentile(params_df["slope"][1:], 95)
+
+                if (params_df["slope"][0] < lower) and (params_df["p_val"][0] < alpha):
+                    ramp_class = "-"
+                elif (params_df["slope"][0] > upper) and (params_df["p_val"][0] < alpha):
+                    ramp_class = "+"   
+                else:
+                    ramp_class = "UN"
+
+                ramp_df = pd.DataFrame({'cluster_id': [cluster_id],
+                                        'trial_type': [trial_type], 
+                                        'hit_miss_try': [hit_miss_try],
+                                        'track_length': [track_region],
+                                        'ramp_class': [ramp_class],}) 
+                model_params = pd.concat([model_params, ramp_df], ignore_index=True)
+    print(f'completed ramp classification for cluster {cluster_id}') 
+    return model_params
+
+                
+
+ 
+def calculate_ramp_scores_parallel(spike_data, processed_position_data, 
+                          position_data, track_length, save_path,
+                          n_shuffles=100, alpha=0.01, save=False):
+
+    n_cores = multiprocessing.cpu_count()  
+    parallel_model_params = Parallel(n_jobs=n_cores)(delayed(ramp_parallel)(cluster_df, processed_position_data, position_data, track_length, alpha, n_shuffles) for i, cluster_df in spike_data.iterrows())
+    model_params = pd.concat(parallel_model_params, ignore_index=True)  
+    
+    if save: 
+        model_params.to_pickle(save_path+"ramp_classifications.pkl")
+    return model_params 
