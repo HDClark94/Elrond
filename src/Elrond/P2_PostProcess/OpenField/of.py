3212b1b831882af2b84262e5d5cfddb90bcc6b25
chrishalcrow <chrishalcrow@gmail.com>
2024-10-30 11:42:17 +0000

    Rearrange of.process to make rate maps before computing scores

--- src/Elrond/P2_PostProcess/OpenField/of.py
+++ src/Elrond/P2_PostProcess/OpenField/of.py
@@ -20,17 +20,19 @@ def process(recording_path, processed_path, dlc_position_data, **kwargs):
 
     spike_data_path = processed_path + sorterName+"/spikes.pkl"
     if os.path.exists(spike_data_path):
+
+        output_path = processed_path + sorterName + "/"
         spike_data = pd.read_pickle(spike_data_path)
+
         spike_data = add_spatial_variables(spike_data, position_data)
+        plot_firing_rate_maps(spike_data, output_path)
+
         spike_data = add_scores(spike_data, position_data, position_heat_map)
         spike_data.to_pickle(spike_data_path)
 
-        # make plots
-        output_path = processed_path + sorterName + "/"
+        plot_rate_map_autocorrelogram(spike_data, output_path)
         plot_spikes_on_trajectory(spike_data, position_data, output_path)
         plot_coverage(position_heat_map, output_path)
-        plot_firing_rate_maps(spike_data, output_path)
-        plot_rate_map_autocorrelogram(spike_data, output_path)
         plot_polar_head_direction_histogram(spike_data, position_data, output_path)
         plot_firing_rate_vs_speed(spike_data, position_data, output_path)
         make_combined_figure(spike_data, output_path)
