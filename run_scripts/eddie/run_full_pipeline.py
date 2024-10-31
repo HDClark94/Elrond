7e4e775c997a65ef76b09a56cd2e1ae6f30d104f
chrishalcrow <chrishalcrow@gmail.com>
2024-10-31 10:59:12 +0000

    make pipeline wait for stagein to finish

--- run_scripts/eddie/run_full_pipeline.py
+++ run_scripts/eddie/run_full_pipeline.py
@@ -10,9 +10,12 @@ project_path = sys.argv[4]
 import Elrond
 elrond_path = Elrond.__path__[0]
 
+
 # check if raw recordings are on eddie. If not, stage them
+stagein_name = ""
 if len(os.listdir(project_path + f"data/M{mouse}_D{day}")) > 1:
-    stagein_data(mouse, day, project_path)
+    stagein_name = f"stagein_M{mouse}_D{day}"
+    stagein_data(mouse, day, project_path, job_name = stagein_name)
 
 # Now run full pipeline on eddie
-run_python_script(elrond_path + "/../../run_scripts/eddie/run_pipeline_on_data.py " + mouse + " " + day + " " + sorter_name + " " + project_path, hold_jid=)
+run_python_script(elrond_path + "/../../run_scripts/eddie/run_pipeline_on_data.py " + mouse + " " + day + " " + sorter_name + " " + project_path, hold_jid=stagein_name)
