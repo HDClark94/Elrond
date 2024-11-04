30c6acc69be8e1a191ecb1142fd767f23a70271b
chrishalcrow <chrishalcrow@gmail.com>
2024-11-04 12:37:26 +0000

    add raw_recording_paths to of dlc and behaviour

--- run_scripts/eddie/behaviour.py
+++ run_scripts/eddie/behaviour.py
@@ -1,4 +1,5 @@
 from run_pipeline import do_behavioural_postprocessing
+from Elrond.Helpers.upload_download import get_chronologized_recording_paths
 import sys
 
 mouse = sys.argv[1]
@@ -6,4 +7,5 @@ day = sys.argv[2]
 sorter_name = sys.argv[3]
 project_path = sys.argv[4]
 
-do_behavioural_postprocessing(mouse, day, sorter_name, project_path)
+raw_recording_paths = get_chronologized_recording_paths(project_path, mouse, day)
+do_behavioural_postprocessing(mouse, day, sorter_name, project_path, recording_paths = raw_recording_paths)
