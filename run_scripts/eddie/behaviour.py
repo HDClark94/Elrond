0166467d9b11b37de304169327479733247c7390
chrishalcrow <chrishalcrow@gmail.com>
2024-11-04 12:10:58 +0000

    Add parallel pipeline (do of dlc while sorting)

---
+++ run_scripts/eddie/behaviour.py
@@ -0,0 +1,9 @@
+from ..run_pipeline import do_behavioural_postprocessing
+import sys
+
+mouse = sys.argv[1]
+day = sys.argv[2]
+sorter_name = sys.argv[3]
+project_path = sys.argv[4]
+
+do_behavioural_postprocessing(mouse, day, sorter_name, project_path)
---
+++ run_scripts/eddie/dlc_of1.py
@@ -0,0 +1,9 @@
+from ..run_pipeline import do_dlc_pipeline
+import sys
+
+mouse = sys.argv[1]
+day = sys.argv[2]
+sorter_name = sys.argv[3]
+project_path = sys.argv[4]
+
+do_dlc_pipeline(mouse, day, dlc_of_model_path = project_path + "derivatives/dlc_models/of_cohort12-krs-2024-10-30/", do_of2=False, do_vr=False)
