9a701935ed44c15b4be7cee1b520ab2b62c171ed
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-11-11 17:15:36 +0000

    add gpu job

--- src/Elrond/Helpers/create_eddie_scripts.py
+++ src/Elrond/Helpers/create_eddie_scripts.py
@@ -63,7 +63,7 @@ def make_gpu_python_script(python_arg,  h_rt=None):
     if h_rt is None:
         h_rt = "0:59:59"
 
-    script_content = """#!/bin/bash
+    script_content = f"""#!/bin/bash
 #$ -cwd -q gpu -pe gpu-a100 1 -l rl9=true,h_vmem=30G,h_rt={h_rt} -N gpu_ks
 source /etc/profile.d/modules.sh
 module load anaconda
@@ -97,7 +97,7 @@ def run_gpu_python_script(python_arg, venv=None, cores=None, email=None, h_rt=No
 
     if job_name is None:
         job_name = "run_python"
-    script_content = make_run_python_script(python_arg, h_rt=h_rt)
+    script_content = make_gpu_python_script(python_arg, h_rt=h_rt)
     save_and_run_script(script_content, f"{job_name}" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".sh")
 
     return
