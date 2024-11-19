62641867156e7ee709f0d67c6476e9195e42590f
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-11-19 14:29:20 +0000

    run_zarr_creation bug

--- run_scripts/eddie/run_zarr_creation.py
+++ run_scripts/eddie/run_zarr_creation.py
@@ -45,8 +45,8 @@ run_python_script(
     h_rt = "0:59:00"
     )
 
-paths_on_datastore = [ path_on_datastore + '/' for path_on_datastore in paths_on_datastore ]
 folder_names = [path_on_datastore.split('/')[-1] for path_on_datastore in paths_on_datastore]
+paths_on_datastore = [ path_on_datastore + '/' for path_on_datastore in paths_on_datastore ]
 paths_on_eddie = [f"{project_path}data/M{mouse}_D{day}/{folder_name}/" for folder_name in folder_names]
 stageout_dict = dict(zip(paths_on_eddie, paths_on_datastore))
 
