4e7169710dc0ec3b8cc8d8d38722b42c6ed833b6
chrishalcrow <57948917+chrishalcrow@users.noreply.github.com>
2024-12-05 11:48:18 +0000

    update_run_theta_pipeline

--- run_scripts/eddie/run_theta_pipeline.py
+++ run_scripts/eddie/run_theta_pipeline.py
@@ -1,14 +1,13 @@
 import sys
 import os
 from Elrond.Helpers.create_eddie_scripts import stagein_data, run_python_script, run_stageout_script, run_gpu_python_script
-from Elrond.Helpers.upload_download import get_chronologized_recording_paths, get_session_names
+from Elrond.Helpers.upload_download import get_session_names, chronologize_paths
 from pathlib import Path
 import Elrond
 
 mouse = sys.argv[1]
 day = sys.argv[2]
-sorter_name = sys.argv[3]
-project_path = sys.argv[4]
+project_path = sys.argv[3]
 
 elrond_path = Elrond.__path__[0]
 
@@ -20,6 +19,9 @@ paths_on_datastore = []
 stagein_job_name = None
 
 filenames_path = project_path + f"data/M{mouse}_D{day}/data_folder_names.txt"
+if Path(filenames_path).exists():
+    with open(filenames_path) as f:
+        paths_on_datastore = f.read().splitlines()
     
 stagein_job_name = f"stagein_M{mouse}_D{day}"
 
@@ -34,29 +36,31 @@ if len(os.listdir(data_path)) < 2:
         else:
             stagein_data(mouse, day, project_path, job_name = stagein_job_name + "_" + str(a), which_rec=a)
 
+session_names = get_session_names(chronologize_paths(paths_on_datastore))
+
 mouseday_string = "M" + mouse + "_" + day + "_"
-theta_job_name = mouseday_string + "theta"
-out_job_name = mouseday_string + "out"
 
-import time
-time.sleep(60)
+theta_job_name = mouseday_string + "th"
+out_job_name = mouseday_string + "out_th"
 
+theta_job_names = [theta_job_name + "_" + session_name for session_name in session_names]
+hold_theta_job_names = ""
+for theta_job_name in theta_job_names:
+    hold_theta_job_names += theta_job_name + ","
 
-raw_recording_paths = get_chronologized_recording_paths(project_path, mouse, day)
-session_names = get_session_names(raw_recording_paths)
-
+# Run theta phase
 for a, session_name in enumerate(session_names):
     # Run theta phase
     run_python_script(
-        elrond_path + "/../../run_scripts/eddie/run_theta_phase.py " + mouse + " " + day + " " + project_path + " " + a,
-        hold_jid = stagein_job_name + "_0,"+stagein_job_name + "_1,"+stagein_job_name + "_2",
-        job_name = theta_job_name + session_name,
-        cores=4,
+        elrond_path + "/../../run_scripts/eddie/run_theta_phase.py " + mouse + " " + day + " " + project_path + " " + str(a),
+        hold_jid = stagein_job_name + "_0,"+stagein_job_name + "_1,"+stagein_job_name + "_2"+stagein_job_name + "_3",
+        job_name = theta_job_name + "_" + session_name,
+        cores=5,
     )
 
 run_stageout_script({
     project_path + "derivatives/M"+mouse+"/D"+day+"/": "/exports/cmvm/datastore/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/Chris/Cohort12/derivatives/M"+mouse+"/D"+day+"/"
     },
-    hold_jid = theta_job_name + "vr,"+theta_job_name + "of1,"+theta_job_name + "of2",
+    hold_jid = hold_theta_job_names,
     job_name = out_job_name
 )
